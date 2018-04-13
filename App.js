c418d80861385d13c01482a228fea7013c4ea4e8
uri <uri.valevski@gmail.com>
2018-04-13 18:24:36 +0300

    basic node e2e

--- App.js
+++ App.js
@@ -1,8 +1,48 @@
 import React, { Component } from 'react';
-import { Platform, StyleSheet, Text, View } from 'react-native';
+import {
+  Platform,
+  StyleSheet,
+  Button,
+  Text,
+  View,
+  TextInput
+} from 'react-native';
+import nodejs from 'nodejs-mobile-react-native';
 
 export default class App extends Component {
+  constructor() {
+    super();
+    this.state = { text: 'initial sent', recieved: 'initial recieved' };
+    nodejs.start('main.js');
+    nodejs.channel.addListener(
+      'message',
+      msg => {
+        this.setState({ recieved: msg });
+      },
+      this
+    );
+  }
+  submit() {
+    nodejs.channel.send(this.state.text);
+  }
   render() {
-    return <View />;
+    const { text, recieved } = this.state;
+    return (
+      <View>
+        <Text>{recieved}</Text>
+        <TextInput
+          value={text}
+          onChangeText={text => {
+            this.setState({ text });
+          }}
+        />
+        <Button
+          onPress={() => {
+            this.submit();
+          }}
+          title="submit"
+        />
+      </View>
+    );
   }
 }
